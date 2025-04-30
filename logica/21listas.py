4d72c365600a0ef9b9480729a200507b6a777e99
guiguitome <guitomef@gmail.com>
2025-04-29 22:01:20 -0300

    mais_listas

--- logica/21listas.py
+++ logica/21listas.py
@@ -1,12 +1,12 @@
 # Tipo list - mutável, pode ser alterada
 # é como se fosse o array em outras linguagem
 
-# lista = list() #forma não comum
+# lista = list() # forma não comum
 lista_2 = [] #forma mais usada / lista vazia
 
 print(bool(lista_2)) #lista vazia -> falsy
 
-#assim como string, possue índices
+#assim como as strings, listas possue índices
 #         0   1      2            3    4
 #        -5  -4     -3           -2   -1
 lista = [123, True, "Guilherme", 1.2, []] # suporta quaisquer tipos
--- logica/22listas2.py
+++ logica/22listas2.py
@@ -5,14 +5,35 @@ print(numero)
 
 # CRUD - Create Read Update Delete
 # deletando
-del lista[1]
+#del lista[1]
 # todos os itens que estão na frente do item deletado tomarão o índice n - 1, sendo n o valor atual
 
-# adicionando item no final da lista
+# adicionando item no final da lista (o parâmetro é o valor que será adicionado)
 lista.append(5)
 
-#deletando o ultimo item da lista
+#deletando o ultimo item da lista (sem parãmetro)
 lista.pop()
 
 # deletando indice especifico
 lista.pop(2)
+
+ultimo_valor = lista.pop()
+print(lista, "Removido = ", ultimo_valor)
+
+# lista.clear() - limpa a lista
+
+# insert - adicionar elemento na lista no indice selecionado
+# sintaxe - variavel.insert(num do indice, item adicionado)
+
+lista.insert(0, 0)
+
+# concatenar listas
+
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+#lista_c = lista_a + lista_b
+#print(lista_c)
+
+lista_a.extend(lista_b)
+print(lista_a)
+
