78f4edf4652bac26ad68e18008aa72f14db7390a
guiguitome <guitomef@gmail.com>
2025-09-09 11:32:39 -0300

    funções e numpy

--- logica/32cpf.py
+++ logica/32cpf.py
@@ -1,29 +1,29 @@
 # Coletar a soma dos 9 primeiros digitos do CPF, multiplicando cada um dos valores por uma contagem regressiva começando em 10
-cpf = input("")
+import random
 
+nove_digitos = ''
+for i in range(9):
+    nove_digitos += str(random.randint(0, 9))
 cont = 10
 soma = 0
-cpf_tratado = []
-for digito in cpf:
-    if digito == '.':
-        continue
-    elif digito == '-':
-        break
-    else:
-        digito = int(digito)
-        cpf_tratado.append(digito)
-        mult = digito * cont
-        soma = soma + mult
-        cont -= 1
+for digito in nove_digitos:
+    digito = int(digito)
+    mult = digito * cont
+    soma = soma + mult
+    cont -= 1
 
-resto = (soma*10)%11
-primeiro_digito = 0 if resto > 9 else resto  
+resto_1 = (soma*10)%11
+primeiro_digito = 0 if resto_1 > 9 else resto_1  
 print(f'O primeiro dígito do cpf é {primeiro_digito}')
 
+cont = 11
+soma = 0
+for digito in nove_digitos + str(primeiro_digito):
+    soma += (int(digito) * cont)
+    cont -= 1
 
+resto_2 = (soma * 10)%11
+segundo_digito = 0 if resto_2 > 9 else resto_2
+print(f"O segundo dígito do cpf é {segundo_digito}")
 
-
-
-
-
-
+print(f'{nove_digitos}-{primeiro_digito}{segundo_digito}')
---
+++ loop/.ipynb_checkpoints/for-checkpoint.py
@@ -0,0 +1,17 @@
+#loop com for
+# sintaxe
+# for var de controle in range(n) (n irá de zero até n-1)
+
+print("range com apenas um parâmetro (10):")
+for i in range(11):
+    print("valor do i:", i)
+
+print("\n")
+print("range com 2 parâmetros (2, 8)")
+for i in range(2, 8):
+    print("valor do i:", i) # dois parâmetro n, m faz o i ir de n até m-1
+
+print("\n")
+print("range com 3 parâmetros (2, 8, 3)")
+for i in range(2, 8, 3): 
+    print("valor de i:", i) # 3 parâmetro l, m, n: l dita quak numero começa, m a partir de que número para, n incrementa
